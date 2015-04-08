3d1b147d00b5d0352ac22aeba34badafcd859933
Prem <Prem@Prems-MacBook-Air.local>
2015-04-08 03:52:28 -0400

    Event list partial

--- FacebookLogin/EventsViewController.swift
+++ FacebookLogin/EventsViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class EventsViewController: UIViewController {
+class EventsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var eventLabel: UILabel!
+    @IBOutlet var eventTableView: UITableView!
     
     var eventDetailsDict = [String : String] ()
+    var eventDetailsArr = [String]()
+    
+    let textCellIdentifier = "TextCell"
+    let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+        
         if !eventDetailsDict.isEmpty{
             for (key,date) in self.eventDetailsDict{
                 println("Event Name: \(key) is on \(date)")
@@ -25,7 +35,7 @@ class EventsViewController: UIViewController {
             eventLabel.text = "No Events!"
         }
         
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,6 +43,25 @@ class EventsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1 // Since i'm implementing with just one section keeping this as 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return eventDetailsArr.count
+        // Most of the time my data source is an array of something...  will replace with the actual name of the data source
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        // set cell's textLabel.text property
+        // set cell's detailTextLabel.text property
+        let row = indexPath.row
+        cell.textLabel?.text = eventDetailsArr[row]
+//        cell.detailTextLabel?.text = eventDetailsArr[row][1]
+        return cell
+    }
 
     /*
     // MARK: - Navigation
